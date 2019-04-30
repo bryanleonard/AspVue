97fb81a301ae45c9ad32a67cd61577d3691bde04
Bryan Leonard <bleonard@acrtinc.com>
2019-04-30 16:49:41 -0400

    Minor updates, showing api data.

--- ClientApp/components/App.vue
+++ ClientApp/components/App.vue
@@ -4,16 +4,34 @@
         <p>
             The time is now: {{ time }}
         </p>
+        
+        <p>The current users of our system are:</p>
+        <ul>
+            <li v-for="user in users" :key="user.userName">
+                {{ user.fullName }} - {{ user.userName }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
     name: 'app',
-    data () {
+    data() {
         return {
+            users: [],
             time: new Date().toString()
         }
+    },
+    mounted () {
+        fetch('/api/users')
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                this.users = data
+            })
     }
+
 }
 </script>
